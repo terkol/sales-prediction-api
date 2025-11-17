5251ce820b0374ac151968bb8600754bbb79c505
terkol <tero0408@gmail.com>
2025-11-17 21:03:31 +0200

    added print statement

--- fetch_google_trends.py
+++ fetch_google_trends.py
@@ -9,12 +9,15 @@ def fetch_normalized_trends(word: str="pizza", tf: str="2025-01-01 2025-12-31") 
     df = pytrends.interest_over_time().reset_index()
     return df
 
-if __name__ == "__main__":
+def save_google_trends(w: str='pizza'):
     data_dir = os.path.dirname(os.path.dirname(__file__))+'\\data'
-    print(dir)
-    df = fetch_normalized_trends(word='fizza')
-    df['trend'] = df['fizza']
+    df = fetch_normalized_trends(word=w)
+    df['trend'] = df[w]
     df['times'] = df['date']
-    df = df.drop(columns=['isPartial', 'fizza', 'date'])
+    df = df.drop(columns=['isPartial', w, 'date'])
     df = df.set_index('times')
     df.to_csv(data_dir + "\\google_trends.csv")
+    print(f"Saved data into file '{data_dir}\\google_trends.csv'")
+
+if __name__ == "__main__":
+    save_google_trends('pizza')
